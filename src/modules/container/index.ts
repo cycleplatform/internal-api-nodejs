5127451c98dba6b40b5872178832c34932c5f56a
Alexander Mattoni <amattoni@petrichor.io>
2020-01-21 15:26:02 -0800

    add environment egress /container calls

--- src/modules/container/index.ts
+++ src/modules/container/index.ts
@@ -1,2 +1,2 @@
 export * from "./container";
-export * from "./certificate";
+export * from "./certificates";
--- src/modules/environment/environment.ts
+++ src/modules/environment/environment.ts
@@ -1,10 +1,22 @@
 import { makeRequest } from "common/request";
-import { Environments, Containers } from "@cycleplatform/cycle-api";
+import {
+  Environments,
+  Containers,
+  Infrastructure,
+  Structs,
+} from "@cycleplatform/cycle-api";
 
 /** Retrieve information about the environment this instance is running in */
 export async function getEnvironment() {
   return makeRequest<Environments.Single>({
     path: "/environment",
+  });
+}
+
+/** Retrieve all containers running in the same environment as this instance */
+export async function getEnvironmentContainers() {
+  return makeRequest<Containers.Single>({
+    path: "/environment/containers",
   });
 }
 
@@ -15,4 +27,15 @@ export async function getEnvironmentInstances() {
   });
 }
 
-export async function getEnvironmentEgressGateways() {}
+/** Retrieve the egress gateways that have been configured for the load balancer that this instance is behind */
+export async function getEnvironmentEgressGateways() {
+  return makeRequest<Environments.Services.Collection>({
+    path: "/environment/egress-gateways",
+  });
+}
+
+export async function getEnvironmentIPs() {
+  return makeRequest<Structs.CollectionDoc<Infrastructure.IPs.IP>>({
+    path: "/environment/ips",
+  });
+}
